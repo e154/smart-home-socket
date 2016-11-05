607e2b6095a44e2ad0588b39696dc99c54f4ebed
delta54 <support@e154.ru>
2016-11-05 22:48:26 +0700

    bug fix

--- socket/common.h
+++ socket/common.h
@@ -14,8 +14,8 @@
 #define set_input(portdir, pin) portdir &= ~(1<<pin)
 #define set_output(portdir, pin) portdir |= (1<<pin)
 #define invert(portdir, pin) portdir ^= (1<<pin)
-#define line_is_high(portdir, pin) ((portdir & (1<<pin)) != 0)
-#define line_is_low(portdir, pin) ((portdir & (1<<pin)) == 0)
+#define line_is_high(port, pin) ((port & (1<<pin)) != 0)
+#define line_is_low(port, pin) ((port & (1<<pin)) == 0)
 
 inline void removeSubstring(char *s,const char *toremove)
 {
